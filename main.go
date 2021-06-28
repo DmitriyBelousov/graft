00c2b531226339399cac894dd5208fd0084c392b
belousov.dmitriy <belousov.dmitriy@rt.ru>
2021-06-29 00:04:57 +0300

    status

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/nats-io/nats.go"
 	"net/http"
+	"time"
 
 	"github.com/nats-io/graft"
 )
 
 func main() {
+	var port string
+	flag.StringVar(&port, "port", "8080", "set up port")
+	flag.Parse()
+
 	fmt.Println("hello kek")
 
-	ci := graft.ClusterInfo{Name: "health_manager", Size: 3}
+	ci := graft.ClusterInfo{Name: "ys5zd84St3FA6AIwZYKtVx", Size: 3}
 
 	opts := nats.GetDefaultOptions()
 	rpc, err := graft.NewNatsRpc(&opts)
@@ -25,22 +31,36 @@ func main() {
 	handler := graft.NewChanHandler(stateChangeChan, errChan)
 
 	node, err := graft.New(ci, handler, rpc, "/tmp/graft.log")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go func() {
+		for{
+		fmt.Println(node.State())
+		time.Sleep(time.Second * 5)
+		}
+	}()
 
 	if node.State() == graft.LEADER {
 		// Process as a LEADER
+		fmt.Println("leader elected")
 	}
 
-	select {
-	case sc := <-stateChangeChan:
-		// Process a state change
-		fmt.Println(sc.To.String())
-	case err := <-errChan:
-		// Process an error, log etc.
-		fmt.Println(err)
-	}
+	go func() {
+		select {
+		case sc := <-stateChangeChan:
+			// Process a state change
+			fmt.Println("change state to - " + sc.To.String())
+		case err := <-errChan:
+			// Process an error, log etc.
+			fmt.Println(err)
+		}
+	}()
 
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
